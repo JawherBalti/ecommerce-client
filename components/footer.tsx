4e074a19057c2349e9e3de6fb1f92f2872f99561
jawherbalti <jawher-balti@live.com>
2023-09-24 10:59:29 +0100

    final version

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const footer = () => {
+    return (
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-sm text-black">
+                    &copy; 2023 Store name, Inc. All rights reserved
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default footer
