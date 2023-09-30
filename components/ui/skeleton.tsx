6c08916e6aa4ad2427a8ab4a0333268698ac0d1b
jawherbalti <jawher-balti@live.com>
2023-09-30 14:08:52 +0100

    loading effect

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -6,7 +6,7 @@ const Skeleton = ({
 }: React.HTMLAttributes<HTMLDivElement>) => {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-neutral-200", className)}
+      className={cn("animate-pulse rounded-md bg-zinc-400", className)}
       {...props}
     />
   )
