7fe3b34041238ad809cb54437eb89672866674c9
jawherbalti <jawher-balti@live.com>
2023-10-03 16:53:27 +0100

    get new data every request instead of caching

--- actions/get-store.tsx
+++ actions/get-store.tsx
@@ -3,7 +3,9 @@ import { Store } from "@/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL2}/stores`
 
 const getStore = async ():Promise<Store> => {
-    const res = await fetch(URL)
+    const res = await fetch(URL, {
+       cache:"no-store" 
+    })
     return res.json()
 }
 
